f4e8b40c56804d358f2bfda9fc3bf27caca50cd7
Rich Leland <rich@richleland.com>
2011-08-03 14:07:31 -0400

    [VII-30, VII-32] tweaked model string validation

--- feedmapper/parsers.py
+++ feedmapper/parsers.py
@@ -7,18 +7,18 @@ class XMLParser(object):
     def __init__(self, mapping):
         self.mapping = mapping
 
-    def validate_model_format(self, model_strings):
-        "Validate that the models in the JSON mapping are in the format app.model."
-        for model_string in model_strings:
-            if not '.' in model_string or model_string.count('.') > 1:
-                return False
+    def validate_model_format(self, model_string):
+        "Validate that a model in the JSON mapping are in the format app.model."
+        if not '.' in model_string or model_string.count('.') > 1:
+            return False
         return True
 
     def grab_models(self):
-        raw_models = self.mapping.data_map['models'].keys()
-        if not self.validate_model_format(raw_models):
-            raise
-        return [get_model(*model.split('.')) for model in raw_models]
+        model_strings = self.mapping.data_map['models'].keys()
+        for model_string in model_strings:
+            if not self.validate_model_format(model_string):
+                raise ValueError("Invalid model format in JSON mapping: %s" % model_string)
+        return [get_model(*model_string.split('.')) for model_string in model_strings]
 
     def parse(self):
         """
