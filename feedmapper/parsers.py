ef905b58b66831820c678f8c86c4464363a0e6a4
Rich Leland <rich@richleland.com>
2011-08-04 09:06:52 -0400

    [VII-30, VII-32] Basics of one-to-one mapping working.

--- feedmapper/parsers.py
+++ feedmapper/parsers.py
@@ -1,5 +1,35 @@
+from lxml import etree
+from StringIO import StringIO
+
 from django.db.models import get_model
 
+
+XML_DUMMY = """<?xml version="1.0" ?>
+<auth>
+    <users>
+        <user>
+            <id>2</id>
+            <username>richleland</username>
+            <first_name>Rich</first_name>
+            <last_name>Leland</last_name>
+            <email>rleland@ngs.org</email>
+        </user>
+        <user>
+            <id>3</id>
+            <username>jtk</username>
+            <first_name>Captain</first_name>
+            <last_name>Kirk</last_name>
+            <email>jkirk@enterprise.org</email>
+        </user>
+    </users>
+    <groups>
+        <group>
+            <id>2</id>
+            <name>Cool people</name>
+        </group>
+    </groups>
+</auth>
+"""
 
 class XMLParser(object):
     "Basic XML to Django model parser"
@@ -23,23 +53,30 @@ class XMLParser(object):
                 instance[model_field] = mapped_to # lots of magic in mapped_to
             instance.save()
         """
+        # this will change to etree.parse(mapping.data_url)
+        tree = etree.parse(StringIO(XML_DUMMY))
+        root = tree.getroot()
+
         model_mappings = self.mapping.data_map['models']
         for model_string, configuration in model_mappings.items():
             if not self.validate_model_format(model_string):
                 raise ValueError("Invalid model format in JSON mapping: %s" % model_string)
-            model = get_model(*model_string.split("."))
-            identifier = configuration['identifier']
+            model = get_model(*model_string.split('.'))
+            identifier = configuration.get('identifier')
+            node_path = configuration['nodePath'].replace('.', '/')
             fields = configuration['fields']
-            instance = model()
-            for field, target in fields.items():
-                # perform logic to determine type of mapping (one to one, one to many, one to transformer)
-                if isinstance(target, basestring):
-                    print "%s is mapped to one field" % field
-                elif isinstance(target, list):
-                    print "%s is mapped to many fields" % field
-                elif isinstance(target, dict):
-                    print "%s is mapped using a transformer" % field
-                setattr(instance, field, target)
-            #instance.save()
-            print instance
+            nodes = root.xpath(node_path)
+            for node in nodes:
+                instance = model()
+                for field, target in fields.items():
+                    if field != identifier:
+                        if isinstance(target, basestring):
+                            value = node.xpath(target)[0].text
+                            print "%s is mapped to one field: %s" % (field, value)
+                        elif isinstance(target, list):
+                            print "%s is mapped to many fields" % field
+                        elif isinstance(target, dict):
+                            print "%s is mapped using a transformer" % field
+                        setattr(instance, field, value)
+                instance.save()
 
