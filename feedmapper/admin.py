15e5338a4ec80c8bd54da977999c8d09cf812d51
Rich Leland <rich@richleland.com>
2011-08-11 15:18:36 -0400

    [VII-37] more friendly admin interface

--- feedmapper/admin.py
+++ feedmapper/admin.py
@@ -4,6 +4,16 @@ from .models import Mapping
 
 
 class MappingAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = (
+        ("Mapping details", {
+            'fields': ('label', 'source', 'parser', 'purge', 'data_map')
+        }),
+        ("Parsing results", {
+            'fields': ('parse_attempted', 'parse_succeeded', 'parse_log')
+        })
+    )
+    list_display = ('label', 'parser', 'purge', 'parse_attempted', 'parse_succeeded')
+    list_filter = ('parser', 'parse_succeeded')
+    readonly_fields = ('parse_log',)
 
 admin.site.register(Mapping, MappingAdmin)
